09a2338acff46a68cfc73a1595987d6a25db5f4c
Sirivally0108 <singarisirivally987@gmail.com>
2026-07-23 09:33:07 +0530

    Add files via upload

---
+++ Data_Analytics/plotting.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = {
+    'Name': ['Arun', 'Bala', 'Chitra', 'Divya', 'Ezhil', 'Fathima'],
+    'Department': ['CSE', 'ECE', 'CSE', 'ECE', 'CSE', 'ECE'],
+    'Marks': [85, 78, 92, 88, 75, 95]
+}
+df = pd.DataFrame(data)
+plt.figure(figsize=(8,5))
+plt.bar(df['Name'], df['Marks'])
+plt.title("Student Marks")
+plt.xlabel("Student Name")
+plt.ylabel("Marks")
+plt.show()
+grouped_df = df.groupby('Department')['Marks'].mean()
+grouped_df.plot(kind='bar')
+plt.title("Average Marks by Department")
+plt.xlabel("Department")
+plt.ylabel("Average Marks")
+plt.show()
