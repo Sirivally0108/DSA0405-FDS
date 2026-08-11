d43c89c80c889182141c968a0c0fce89abc97bae
Sirivally0108 <singarisirivally987@gmail.com>
2026-08-11 10:52:47 +0530

    Added files

--- UNIT-3/Descriptive_Statistics/Des_Statistics1.py
+++
@@ -1,5 +0,0 @@
-data = [10, 20, 30, 40, 50]
-
-mean_val = sum(data) / len(data)
-
-print("Mean:", mean_val)
--- UNIT-3/Descriptive_Statistics/Des_Statistics4.py
+++
@@ -1,5 +0,0 @@
-data = [15, 8, 22, 42, 10]
-
-range_val = max(data) - min(data)
-
-print("Range:", range_val)
